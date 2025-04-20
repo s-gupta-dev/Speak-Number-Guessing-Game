07456752da964b1f5ed35e39210ec8ee7dde5baf
Sucheta Gupta <sucheta.gupta.dev@gmail.com>
2025-04-20 01:40:53 -0400

    CUSTOM CHANGE: changing the color of the words to light pastel purple

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ recognition.start();
 function onSpeak(event) {
   const msg = event.results[0][0].transcript;
   writeMessage(msg);
+  checkNumber(msg);
 }
 
 // Listen to and handle speech event
@@ -32,4 +33,71 @@ function writeMessage(msg) {
   span.classList.add("box");
   span.textContent = msg;
   msgEl.append(div, span);
+}
+
+// Check msg against the secret number
+function checkNumber(msg) {
+  const wordToNumber = {
+    one: 1,
+    won: 1,
+    two: 2,
+    to: 2,
+    too: 2,
+    three: 3,
+    four: 4,
+    for: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    ate: 8,
+    nine: 9,
+    ten: 10,
+  };
+
+  if (wordToNumber[msg]) {
+    console.log(`adjusting ${msg} to ${wordToNumber[msg]}`);
+    msg = wordToNumber[msg];
+  }
+
+  let num = Number(msg);
+
+  // Check if the spoken content is a valid number
+  if (Number.isNaN(num)) {
+    const div = document.createElement("div");
+    div.textContent = "That is not a valid number";
+    msgEl.append(div);
+    return;
+  }
+
+  // Check if it's in range
+  if (num < 1 || num > 100) {
+    const div = document.createElement("div");
+    div.textContent = "Number must be between 1 and 100";
+    msgEl.append(div);
+    return;
+  }
+
+  // Check the number and provide feedback
+  if (num === randomNum) {
+    const h2 = document.createElement("h2");
+    h2.textContent = `Congrats! You have guessed the number! It was ${num}`;
+
+    const button = document.createElement("button");
+    button.classList.add("play-again");
+    button.id = "play-again";
+    button.textContent = "Play Again";
+
+    msgEl.append(h2, button);
+  } else if (num > randomNum) {
+    const div = document.createElement("div");
+    div.textContent = "GO LOWER";
+
+    msgEl.append(div);
+  } else {
+    const div = document.createElement("div");
+    div.textContent = "GO HIGHER";
+
+    msgEl.append(div);
+  }
 }
